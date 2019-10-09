a21d7d9a57e1cbbc099f56727a1fd4bc84379f9a
Murat AKMAMEDAU <muratx10@gmail.com>
2019-10-10 00:53:21 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let array = expression.split('*');
+  let countFives = 0;
+  let allNumbers = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].includes('!!')) {
+      let counter = parseInt(array[i]);
+      while (counter > 1) {
+        allNumbers.push(counter);
+        counter -= 2;
+      }
+
+    } else {
+      let counter = parseInt(array[i]);
+      while (counter > 1) {
+        allNumbers.push(counter);
+        counter--;
+      }
+    }
+  }
+
+  if (allNumbers.includes(2)) {
+    for (let item of allNumbers) {
+      while (item % 5 === 0) {
+        countFives++;
+        item /= 5;
+      }
+    }
+  } else {
+    countFives = 0;
+  }
+  
+  return countFives;
 }
